ec9f91e49a053a6792e68d9ce53002a30cd3c833
diego <diego.correa@ufba.br>
2024-03-11 10:57:00 +0100

    adding evaluation metrics to DAE

--- shell/step5_steck.sh
+++ shell/step5_steck.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd ./code && python3 step5_metrics.py from_file=YES file_name=step5_steck > ../logs/step5_steck.log 2>&1 & disown
+cd ./pierre_in_frame && python3 step5_metrics.py from_file=YES file_name=step5_steck > ../logs/step5_steck.log 2>&1 & disown
