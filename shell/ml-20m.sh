718233ecc533e768fa4935f099334482970bb176
diego <diego.correa@ufba.br>
2024-04-12 16:04:26 +0200

    changing environment config

--- shell/ml-20m.sh
+++ shell/ml-20m.sh
@@ -5,4 +5,4 @@ python3 step1_preprocessing.py from_file=YES file_name=step1_ohe_ml-20m &&
 python3 step2_searches.py from_file=YES file_name=ml-20m &&
 python3 step3_processing.py from_file=YES file_name=ml-20m &&
 python3 step4_postprocessing.py from_file=YES file_name=ml-20m_steck &&
-python3 step5_metrics.py from_file=YES file_name=ml-20m_steck
+python3 step5_metrics.py from_file=YES file_name=ml-20m_steck > ../logs/step_steck.log
