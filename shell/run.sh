4700d915206d6cb861234f9f744ab5a4ddc36352
diego <diego.correa@ufba.br>
2024-04-19 15:54:37 +0200

    changing environment variables

--- shell/run.sh
+++ shell/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd ./pierre_in_frame &&
+python3 step2_searches.py from_file=YES file_name=step2 &&
 python3 step3_processing.py from_file=YES file_name=step3 &&
 python3 step4_postprocessing.py from_file=YES file_name=step4 &&
 python3 step5_metrics.py from_file=YES file_name=step5
