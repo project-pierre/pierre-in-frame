b1bb40e2e2835013168e7e668a1377fa2e1b26e4
diego <diego.correa@ufba.br>
2024-04-19 15:48:04 +0200

    changing environment config

--- shell/run.sh
+++ shell/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd ./pierre_in_frame &&
+python3 step3_processing.py from_file=YES file_name=step3 &&
 python3 step4_postprocessing.py from_file=YES file_name=step4 &&
 python3 step5_metrics.py from_file=YES file_name=step5
