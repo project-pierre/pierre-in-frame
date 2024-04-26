c937cd6c24c30f71f4967c15fa0e48af13bcd416
diego <diego.correa@ufba.br>
2024-04-26 12:34:29 +0200

    preparing to run

--- shell/ml-1m.sh
+++ shell/ml-1m.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd ./pierre_in_frame &&
+python3 step1_preprocessing.py from_file=YES file_name=ml &&
+python3 step1_preprocessing.py from_file=YES file_name=ml-1m_distribution &&
+python3 step1_preprocessing.py from_file=YES file_name=ml-1m_ohe &&
 python3 step2_searches.py from_file=YES file_name=ml-1m &&
 python3 step3_processing.py from_file=YES file_name=ml-1m &&
 python3 step4_postprocessing.py from_file=YES file_name=ml-1m &&
