e6c33661b5352b78ed1fd04b0f4a64fe601b0ac5
diego <diego.correa@ufba.br>
2024-05-03 15:16:27 +0200

    changing environment variables

--- shell/weekend.sh
+++ shell/weekend.sh
@@ -2,14 +2,11 @@
 cd ./pierre_in_frame &&
 
 python3 step3_processing.py from_file=YES file_name=lfm-2b-subset &&
-
-python3 step1_preprocessing.py from_file=YES file_name=step1_lfm_distribution &&
-python3 step1_preprocessing.py from_file=YES file_name=step1_ohe_lfm &&
-
+python3 step1_preprocessing.py from_file=YES file_name=lfm-2b-subset_distribution &&
+python3 step1_preprocessing.py from_file=YES file_name=lfm-2b-subset_ohe &&
 python3 step2_searches.py from_file=YES file_name=deep_ae_food &&
 python3 step2_searches.py from_file=YES file_name=deep_ae_ml-20m &&
 python3 step2_searches.py from_file=YES file_name=deep_ae_lfm-2b-subset &&
-
 python3 step3_processing.py from_file=YES file_name=deep_ae_food &&
 python3 step3_processing.py from_file=YES file_name=deep_ae_ml-20m &&
 python3 step3_processing.py from_file=YES file_name=deep_ae_lfm-2b-subset
