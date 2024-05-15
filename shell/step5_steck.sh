dbe8af1cacf1c9f63d73387fd8d07192e8e91ae5
diego <diego.correa@ufba.br>
2024-05-15 15:54:14 +0200

    changing environment variables

--- shell/step5_steck.sh
+++ shell/step5_steck.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cd ./pierre_in_frame && python3 step5_metrics.py from_file=YES file_name=step5_steck > ../logs/step5_steck.log 2>&1 & disown
+python3 step5_metrics.py from_file=YES file_name=lfm-2b-subset &&
+python3 step5_metrics.py from_file=YES file_name=lfm-2b-subset_p
