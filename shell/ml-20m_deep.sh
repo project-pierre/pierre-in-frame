384a66731f9247f5e8a2291d716ff67413171a13
diego <diego.correa@ufba.br>
2024-05-16 10:33:45 +0200

    changing environment variables

--- shell/ml-20m_deep.sh
+++ shell/ml-20m_deep.sh
@@ -3,4 +3,4 @@ cd ./pierre_in_frame &&
 python3 step2_searches.py from_file=YES file_name=deep_ae_ml-20m &&
 python3 step3_processing.py from_file=YES file_name=deep_ae_ml-20m &&
 python3 step4_postprocessing.py from_file=YES file_name=deep_ae_ml-20m &&
-python3 step5_metrics.py from_file=YES file_name=deep_ae_ml-20m &&
+python3 step5_metrics.py from_file=YES file_name=deep_ae_ml-20m
