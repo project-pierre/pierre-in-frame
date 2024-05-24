27910869d5c4e8075ddd3c6a9331bf76801e5076
diego <diego.correa@ufba.br>
2024-05-24 12:32:06 +0200

    changing environment variables

--- shell/lfm_deep.sh
+++ shell/lfm_deep.sh
@@ -5,4 +5,8 @@ python3 step3_processing.py from_file=YES file_name=deep_ae_lfm-2b-subset &&
 python3 step4_postprocessing.py from_file=YES file_name=deep_ae_lfm-2b-subset &&
 python3 step5_metrics.py from_file=YES file_name=deep_ae_lfm-2b-subset &&
 python3 step6_protocol.py from_file=YES file_name=lfm-2b-subset &&
-python3 step7_charts_tables.py from_file=YES file_name=lfm-2b-subset
+python3 step7_charts_tables.py from_file=YES file_name=lfm-2b-subset &&
+python3 step2_searches.py from_file=YES file_name=food_conformity &&
+python3 step2_searches.py from_file=YES file_name=lfm-2b-subset_conformity &&
+python3 step2_searches.py from_file=YES file_name=ml-20m_conformity
+
