b43f4d989b1a7153efea2d778f53667a4dde90a4
lowe <diegocorrea.cc@gmail.com>
2024-06-18 09:40:35 -0300

    reducing the number of using cores

--- shell/run.sh
+++ shell/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd ./pierre_in_frame &&
-python3 step2_searches.py from_file=YES file_name=deep_ae_lfm-2b-subset &&
-python3 step3_processing.py from_file=YES file_name=deep_ae_lfm-2b-subset &&
-python3 step4_postprocessing.py from_file=YES file_name=deep_ae_lfm-2b-subset &&
-python3 step5_metrics.py from_file=YES file_name=deep_ae_lfm-2b-subset &&
-python3 step6_protocol.py from_file=YES file_name=lfm-2b-subset &&
-python3 step7_charts_tables.py from_file=YES file_name=lfm-2b-subset &&
+#python3 step2_searches.py from_file=YES file_name=deep_ae_lfm-2b-subset &&
+#python3 step3_processing.py from_file=YES file_name=deep_ae_lfm-2b-subset &&
+#python3 step4_postprocessing.py from_file=YES file_name=deep_ae_lfm-2b-subset &&
+#python3 step5_metrics.py from_file=YES file_name=deep_ae_lfm-2b-subset &&
+#python3 step6_protocol.py from_file=YES file_name=lfm-2b-subset &&
+#python3 step7_charts_tables.py from_file=YES file_name=lfm-2b-subset &&
 python3 step5_metrics.py from_file=YES file_name=step5 &&
 python3 step6_protocol.py from_file=YES file_name=step6 &&
 python3 step7_charts_tables.py from_file=YES file_name=step7 &&
