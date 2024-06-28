ddd8db848ae9ff8676c14bf8148b12e7fc8c7fc1
lowe <diegocorrea.cc@gmail.com>
2024-06-28 10:08:44 -0300

    reducing the number of using cores

--- shell/mace.sh
+++ shell/mace.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ./pierre_in_frame &&
-python3 step6_protocol.py from_file=YES file_name=step6 &&
-python3 step7_charts_tables.py from_file=YES file_name=step7
+python3 step6_protocol.py from_file=YES file_name=mace &&
+python3 step7_charts_tables.py from_file=YES file_name=mace
